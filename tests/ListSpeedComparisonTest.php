99eac0f1206adafa3ba3618ae158d3005e8950f9
Michael Garate <mpgarate@gmail.com>
2015-02-21 15:09:15 -0500

    add time comparison test

---
+++ tests/ListSpeedComparisonTest.php
@@ -0,0 +1,40 @@
+<?php
+
+class ListSpeedComparisonTest extends PHPUnit_Framework_TestCase
+{
+  public function testListGetSpeed()
+    {
+      $linkedList = new LinkedList;
+      $arrayList = new ArrayList;
+
+      for ($i = 0; $i < 10000; $i++) {
+        $linkedList->add("Message # $i");
+      }
+
+      for ($i = 0; $i < 10000; $i++) {
+        $arrayList->add("Message # $i");
+      }
+
+      $linkedListTimeStart = microtime(true);
+
+      for ($i = 0; $i < 10000; $i++) {
+        $linkedList->get($i);
+      }
+
+      $linkedListTimeEnd = microtime(true);
+      $linkedListTimeElapsed = $linkedListTimeEnd - $linkedListTimeStart;
+
+      $arrayListTimeStart = microtime(true);
+      for ($i = 0; $i < 10000; $i++) {
+        $arrayList->get($i);
+      }
+
+      $arrayListTimeEnd = microtime(true);
+      $arrayListTimeElapsed = $arrayListTimeEnd - $arrayListTimeStart;
+
+      echo "\n";
+      echo "ArrayList vs LinkedList for i->10,000 get(i)";
+      echo "array: $arrayListTimeElapsed" . "\n";
+      echo "linked: $linkedListTimeElapsed" . "\n";
+    }
+}
